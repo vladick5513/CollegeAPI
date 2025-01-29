912492dd154f5601e83624f3536f50be03df63ba
vlados <2006krickunow@gmail.com>
2025-01-29 15:36:40 +0300

    added annotations for columns in the SQLAlchemy model

--- app/database.py
+++ app/database.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from typing import Annotated
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 from app.config import settings
 
@@ -15,3 +18,9 @@ async_engine = create_async_engine(
 )
 
 async_session_factory = async_sessionmaker(async_engine)
+
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+update_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
