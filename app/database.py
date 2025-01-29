55e6ecae9fa5e737a99fc60911d4129179ed0938
vlados <2006krickunow@gmail.com>
2025-01-29 23:33:44 +0300

    changing the attribute name and reorganizing the code

--- app/database.py
+++ app/database.py
@@ -6,15 +6,7 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, declared_attr, Mapped
 from app.config import settings
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
 
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
-
-    created_at: Mapped[created_at]
-    update_at: Mapped[update_at]
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
@@ -27,6 +19,16 @@ async_session_factory = async_sessionmaker(async_engine)
 
 int_pk = Annotated[int, mapped_column(primary_key=True)]
 created_at = Annotated[datetime, mapped_column(server_default=func.now())]
-update_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
 str_null_true = Annotated[str, mapped_column(nullable=True)]
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
