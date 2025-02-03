c4aa144afedbc9591394c6897ac36bfdda268856
vlados <2006krickunow@gmail.com>
2025-02-03 19:35:11 +0300

    add relationship students and extend_existing

--- app/majors/models.py
+++ app/majors/models.py
@@ -1,11 +1,19 @@
 from sqlalchemy import text
-from sqlalchemy.orm import Mapped, mapped_column
-
-from app.database import Base, int_pk, str_uniq, str_null_true
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base, str_uniq, int_pk, str_null_true
 
 
 class Major(Base):
     id: Mapped[int_pk]
     major_name: Mapped[str_uniq]
     major_description: Mapped[str_null_true]
-    count_students: Mapped[int] = mapped_column(server_default=text("0"))
+    count_students: Mapped[int] = mapped_column(server_default=text('0'))
+
+    students: Mapped[list["Student"]] = relationship("Student", back_populates="major")
+    extend_existing = True
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
+
+    def __repr__(self):
+        return str(self)
