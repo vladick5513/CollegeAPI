97b751d5716db81122d39a1f2facc8256b9f27d1
vlados <2006krickunow@gmail.com>
2025-02-04 18:34:04 +0300

    created SMajorsAdd model for majors

---
+++ app/majors/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+
+class SMajorsAdd(BaseModel):
+    major_name: str = Field(..., description="Название факультета")
+    major_description: str = Field(None, description="Описание факультета")
+    count_students: int = Field(0, description="Количество студентов")
