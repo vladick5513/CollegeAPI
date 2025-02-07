05f03cd0a452aebcea13f20920d26ae33993cf61
vlados <2006krickunow@gmail.com>
2025-02-07 13:35:03 +0300

    еnhanced StudentDAO with major count update and add_student method

--- app/students/dao.py
+++ app/students/dao.py
@@ -1,9 +1,19 @@
-from sqlalchemy import select
+from sqlalchemy import select, event, update, delete
 from sqlalchemy.orm import joinedload
 
 from app.dao.base import BaseDAO
 from app.database import async_session_factory
+from app.majors.models import Major
 from app.students.models import Student
+
+@event.listens_for(Student, "after_insert")
+def receive_after_insert(mapper, connection, target):
+    major_id = target.major_id
+    connection.execute(
+        update(Major)
+        .where(Major.id == major_id)
+        .values(count_students = Major.count_students +1)
+    )
 
 
 class StudentDAO(BaseDAO):
@@ -22,3 +32,13 @@ class StudentDAO(BaseDAO):
             student_data = student_info.to_dict()
             student_data["major"] = student_info.major.major_name
             return student_data
+    @classmethod
+    async def add_student(cls, **student_data: dict):
+        async with async_session_factory() as session:
+            async with session.begin():
+                new_student = Student(**student_data)
+                session.add(new_student)
+                await session.flush()
+                new_student_id = new_student.id
+                await session.commit()
+                return new_student_id
