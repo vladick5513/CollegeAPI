028498e0225f22ec5001d1b4642e5d09816c8ace
vlados <2006krickunow@gmail.com>
2025-02-07 13:49:06 +0300

    add update_major_description endpoint for updating major descriptions

--- app/majors/router.py
+++ app/majors/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.majors.dao import MajorsDAO
-from app.majors.schemas import SMajorsAdd
+from app.majors.schemas import SMajorsAdd, SMajorsUpdDesc
 
 router = APIRouter(prefix="/majors", tags=["Работа с факультетами"])
 
@@ -12,4 +12,14 @@ async def add_major(major: SMajorsAdd) -> dict:
         return {"message": "Факультет успешно добавлен!", "major": major}
     else:
         return {"message": "Ошибка при добавлении факультета!"}
+
+@router.put("/update_description/")
+async def update_major_description(major: SMajorsUpdDesc) -> dict:
+    check = await MajorsDAO.update(filter_by={"major_name": major.major_name},
+                                   major_description=major.major_description)
+    if check:
+        return {"message": "Описание факультета успешно обновлено!", "major": major}
+    else:
+        return {"message": "Ошибка при обновлении описания факультета!"}
+
 
