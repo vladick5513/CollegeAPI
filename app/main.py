b508035378ce879bdfd501a29db62370c1a0f8b0
vlados <2006krickunow@gmail.com>
2025-02-22 13:23:14 +0300

    add a user router

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.students.router import router as router_students
 from app.majors.router import router as router_majors
+from app.users.router import router as router_users
 
 app = FastAPI()
 
 app.include_router(router_students)
 app.include_router(router_majors)
+app.include_router(router_users)
