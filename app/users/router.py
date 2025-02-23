437a94430054f759aba4b350a66db84b32f13e6b
vlados <2006krickunow@gmail.com>
2025-02-23 15:22:23 +0300

    Add /login route

--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Response
 
-from app.users.auth import get_password_hash
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserRegister, SUserAuth
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -15,4 +15,14 @@ async def register_user(user_data: SUserRegister):
     user_dict["password"] = get_password_hash(user_data.password)
     await UsersDAO.add(**user_dict)
     return {"message": f'Вы успешно зарегистрированы!'}
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: SUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Неверная почта или пароль")
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="user_access_token", value=access_token,  httponly=True)
+    return {"access_token": access_token, "refresh_token": None}
 
