234b1d159257fed7625c2bc2e65155f34c29c607
vlados <2006krickunow@gmail.com>
2025-02-24 18:52:40 +0300

    add admin user validation function

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Request, HTTPException, status, Depends
 from jose import jwt, JWTError
 from app.config import get_auth_data
 from app.users.dao import UsersDAO
+from app.users.models import User
 
 
 def get_token(request: Request):
@@ -32,3 +33,8 @@ async def get_current_user(token = Depends(get_token)):
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
     return user
+
+async def get_current_admin_user(current_user: User = Depends(get_current_user)):
+    if current_user.is_admin:
+        return current_user
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Недостаточно прав")
