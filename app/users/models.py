1d113c13b3297c9778a863b3b07bd4573daa89f8
vlados <2006krickunow@gmail.com>
2025-02-24 18:53:21 +0300

    refactor User model to set default roles to False

--- app/users/models.py
+++ app/users/models.py
@@ -13,10 +13,10 @@ class User(Base):
     password: Mapped[str]
 
     is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
-    is_student: Mapped[bool] = mapped_column(default=True, server_default=text('false'), nullable=False)
-    is_teacher: Mapped[bool] = mapped_column(default=True, server_default=text('false'), nullable=False)
-    is_admin: Mapped[bool] = mapped_column(default=True, server_default=text('false'), nullable=False)
-    is_super_admin: Mapped[bool] = mapped_column(default=True, server_default=text('false'), nullable=False)
+    is_student: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_teacher: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_super_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
 
     extend_existing = True
 
